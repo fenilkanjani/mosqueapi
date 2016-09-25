3d8eb69f3fd654d8049abe43eee1e139e3c80e59
fenilkanjani <fenilkanjani@gmail.com>
2016-09-25 18:19:19 +0530

    adding query search

--- app.js
+++ app.js
@@ -79,6 +79,43 @@ app.post('/updatetimings', urlencodedParser, function (req, res) {
   });
 });
 
+app.get('/querysearch', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  var location = req.query.query;
+  request.get(
+    {
+      url: 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + location + '&key=' + googleMapKey
+    }, 
+    function (err, response, body) {
+      var bodyjson = JSON.parse(body);
+      var results= [];
+      if (bodyjson) {
+        results = bodyjson.results;
+        var googleIds = [];
+        results.forEach(function (result) {
+          googleIds.push('"' + result.id + '"');
+        });
+        googleIds = googleIds.join(',');
+        googleIds = googleIds || '""';
+        connection.query('select * from mosques where google_id in ('+ googleIds +');', function(err, rows, fields) {
+            if (err) {
+                throw err;
+            }
+            results.forEach(function (currentResult) {
+              rows.some(function (currentRow) {
+                if (currentResult.id === currentRow.google_id) {
+                  currentResult.timings = currentRow.timings;
+                  return true;
+                }
+              });
+            });
+            res.send(JSON.stringify(results));
+        });
+      }
+    }
+  );
+});
+
 var server = app.listen(process.env.PORT || 3000, function () {
     var host = server.address().address;
     var port = server.address().port;
