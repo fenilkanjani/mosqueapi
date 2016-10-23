bbc37ecee65156c56165ad879a2beec84edf353a
fenilkanjani <fenilkanjani@gmail.com>
2016-10-23 17:07:06 +0530

    changing timings from select to input

--- settimings/js/main.js
+++ settimings/js/main.js
@@ -27,23 +27,6 @@
 			$scope.selectedMosque = '';
 			$scope.timings = '';
 
-			$scope.timeList = [
-				'06:00', '06:15', '06:30', '06:45',
-				'07:00', '07:15', '07:30', '07:45',
-				'08:00', '08:15', '08:30', '08:45',
-				'09:00', '09:15', '09:30', '09:45',
-				'10:00', '10:15', '10:30', '10:45',
-				'11:00', '11:15', '11:30', '11:45',
-				'12:00', '12:15', '12:30', '12:45',
-				'13:00', '13:15', '13:30', '13:45',
-				'14:00', '14:15', '14:30', '14:45',
-				'15:00', '15:15', '15:30', '15:45',
-				'16:00', '16:15', '16:30', '16:45',
-				'17:00', '17:15', '17:30', '17:45',
-				'18:00', '18:15', '18:30', '18:45',
-				'19:00', '19:15', '19:30', '19:45'
-			];
-
 			$scope.getMosques = function () {
 				if ($scope.selectedArea) {
 					$http.get('/querysearch?query=mosques in ' + $scope.selectedArea + ',bangalore')
@@ -75,6 +58,16 @@
 			}
 
 			$scope.updateTimings = function () {
+				if (
+					$scope.timings.some(function (timing) {
+						if (!timing || !timing.length || timing.length !== 5 || !/^\d{2}:\d{2}$/.test(timing)) {
+							return true
+						}
+					})
+				) {
+					alert('Timings should have a hh:mm format and should not be empty. Also only numbers 0-9 and \':\' are allowed as input.');
+					return;
+				}
 				$http.post('/updatetimings', {
 					google_id: $scope.selectedMosque,
 					timings: $scope.timings
